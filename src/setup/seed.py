33709117ccab2e9c0b6a8e1dc2877ca60efe68d4
Bruno Vieira Costa <bruno@abstra.app>
2023-02-21 14:25:28 -0300

    adding fake population

--- src/setup/seed.py
+++ src/setup/seed.py
@@ -1,6 +1,45 @@
 from api.db import db
 from models.Customer import Customer
+from models.Project import Project, ProjectMilestone, ProjectMilestoneTask, ProjectMilestoneTaskDependency
+from faker import Faker
+import random
+
+fake = Faker()
 
 
 def run():
-    db.create_tables([Customer])
+    db.create_tables([
+        Customer,
+        Project,
+        ProjectMilestone,
+        ProjectMilestoneTask,
+        ProjectMilestoneTaskDependency
+    ])
+
+    for _ in range(10):
+        customer = Customer.create(
+            name=fake.name(),
+            ein=fake.ein(),
+            email=fake.company_email(),
+            phone=fake.phone_number(),
+            country=fake.country(),
+            zip=fake.zipcode(),
+            address=fake.address()
+        )
+
+        for _ in range(random.randint(5, 10)):
+            project = Project.create(name='Test Project', customer=customer)
+            milestone = ProjectMilestone.create(
+                name=fake.text(20),
+                project=project,
+                description=fake.paragraph(),
+                deadline=fake.date_time()
+            )
+            ProjectMilestoneTask.create(
+                name=fake.text(20),
+                description=fake.paragraph(),
+                deadline=fake.date_time(),
+                start=fake.date_time(),
+                end=fake.date_time(),
+                project_milestone=milestone
+            )
--- src/views/customers/edit.py
+++ src/views/customers/edit.py
@@ -1,9 +1,13 @@
 from models.Customer import Customer
+from models.Project import Project
 import json
+from playhouse.shortcuts import model_to_dict
+import pandas as pd
 
 
 customer_id = __query_params__["id"]
 customer = Customer.get_by_id(customer_id)
+projects = pd.DataFrame([model_to_dict(c) for c in Project.select().where(Project.customer == customer_id)])
 
 
 countries = [
