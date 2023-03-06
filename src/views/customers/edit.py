a1da20556915ae773a924e2481b8ab3f06fa8eec
felipereyel <felipe.reyel@gmail.com>
2023-03-06 14:55:23 -0300

    fix links

--- src/views/customers/edit.py
+++ src/views/customers/edit.py
@@ -3,10 +3,10 @@ from models.Project import Project
 import json
 from playhouse.shortcuts import model_to_dict
 import pandas as pd
-from abstra.dashes import redirect, query_params
+from abstra.dashes import redirect, get_query_params
 
 
-customer_id = query_params["id"]
+customer_id = get_query_params()["id"]
 customer = Customer.get_by_id(customer_id)
 projects = pd.DataFrame(
     [model_to_dict(c) for c in Project.select().where(Project.customer == customer_id)]
--- src/views/projects/edit.py
+++ src/views/projects/edit.py
@@ -1,16 +1,17 @@
 from models.Project import Project
 from playhouse.shortcuts import model_to_dict
 import pandas as pd
-from abstra.dashes import redirect, query_params
+from abstra.dashes import redirect, get_query_params
 
 
-project_id = query_params["id"]
+project_id = get_query_params()["id"]
 project = Project.get_by_id(project_id)
 
 
 def update_project():
     project.save()
     redirect("views/customers/edit", {"id": project.customer_id})
+
 
 board = {
     "type": "kanban-board",
@@ -24,8 +25,10 @@ board = {
                     "name": t.name,
                     "description": t.description,
                     "tags": [],
-                } for t in m.tasks
-            ]
-        } for m in project.milestones
-    ]
+                }
+                for t in m.tasks
+            ],
+        }
+        for m in project.milestones
+    ],
 }
